bbc2950dc87cdd3bf6463f132cc714651c62722e
Peter <pedamain@gmx.at>
2020-11-30 21:30:28 +0100

    refractored

--- src/main/java/ServerImpl.java
+++ src/main/java/ServerImpl.java
@@ -1,13 +1,8 @@
-
-
-
-
 import com.github.Pawedla.Server;
 
 import java.io.*;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-
 
 public class ServerImpl implements Server {
 
@@ -17,18 +12,18 @@ public class ServerImpl implements Server {
 
     //Gets desired Order from Client
     @Override
-    public String bookOrder(String selection) {
-
+    public String bookOrder(Integer number, String[] order, String[] offer) {
 
         try {
-            BufferedReader file = new BufferedReader(new FileReader("save.txt"));
+            new File("save.txt").createNewFile();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("save.txt"));
             StringBuffer inputBuffer = new StringBuffer();
             String line;
-            while ((line = file.readLine()) != null) {
-                inputBuffer.append(line);
+            while ((line = bufferedReader.readLine()) != null) {
+                inputBuffer.append(line + "\n");
             }
-            inputBuffer.append(selection);
-            file.close();
+            bufferedReader.close();
+            inputBuffer.append("nr[" + number + "]-name[" + order[4] + "]-config[" + order[0] + ", " + order[1] + ", " + order[2] + ", " + order[3] + "]-cost[" + offer[1] + "]-supplyer[" + offer[0] + "]-deliveryDate[" + offer[2] + "]\n");
             FileOutputStream fileOut = new FileOutputStream("save.txt");
             fileOut.write(inputBuffer.toString().getBytes());
             fileOut.close();
