c22e00226bbd4f2df9787773eac23b5ed7402ecd
Peter <pedamain@gmx.at>
2020-11-30 23:05:31 +0100

    added comments

--- src/main/java/ServerImpl.java
+++ src/main/java/ServerImpl.java
@@ -10,19 +10,21 @@ public class ServerImpl implements Server {
         UnicastRemoteObject.exportObject(this, 0);
     }
 
-    //Gets desired Order from Client
     @Override
     public String bookOrder(Integer number, String[] order, String[] offer) {
 
         try {
+            //try creating a new file
             new File("save.txt").createNewFile();
             BufferedReader bufferedReader = new BufferedReader(new FileReader("save.txt"));
             StringBuffer inputBuffer = new StringBuffer();
             String line;
+            //appending each line from save.txt to buffered reader
             while ((line = bufferedReader.readLine()) != null) {
                 inputBuffer.append(line + "\n");
             }
             bufferedReader.close();
+            //adding new order
             inputBuffer.append("nr[" + number + "]-name[" + order[4] + "]-config[" + order[0] + ", " + order[1] + ", " + order[2] + ", " + order[3] + "]-cost[" + offer[1] + "]-supplyer[" + offer[0] + "]-deliveryDate[" + offer[2] + "]\n");
             FileOutputStream fileOut = new FileOutputStream("save.txt");
             fileOut.write(inputBuffer.toString().getBytes());
